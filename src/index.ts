6a883b13ee967df1b8ea64112a23d18cd0baf3e1
Max <bazarsadtsogtjargal@gmail.com>
2022-02-09 23:24:13 +0900

    configure express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello 2');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+  })
+);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(8000, () => {
+  console.log('Listening on port: 8000');
+});
