88e53878d72e9bc1895473b1d5b412c87c16974d
Max <bazarsadtsogtjargal@gmail.com>
2022-02-10 15:06:02 +0900

    add typeorm for creating table

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
 
 const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: ['http://localhost:3000'],
-  })
-);
+createConnection().then(() => {
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: ['http://localhost:3000'],
+    })
+  );
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
-});
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+  });
 
-app.listen(8000, () => {
-  console.log('Listening on port: 8000');
+  app.listen(8000, () => {
+    console.log('Listening on port: 8000');
+  });
 });
